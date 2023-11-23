bde9056471ceb6dbb0e9ea7312ffa1ed0cc00247
victoras24 <fin17217@uom.edu.gr>
2023-11-24 01:01:12 +0200

    replaced scroll to hide header with framer motion library

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,35 +1,36 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { NavLink } from "react-router-dom";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import logoImage from "../assets/site-logo.png"
 import Navbar from "./Navbar";
 
 export default function Header() {
 
-    const [scroll, setScroll] = useState(0);
-    const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(false)
+    const { scrollY } = useScroll()
 
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPos = window.scrollY;
-
-            setVisible(currentScrollPos < scroll || currentScrollPos < 100);
-
-            setScroll(currentScrollPos);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [scroll]);
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const previous = scrollY.getPrevious()
+        if (latest > previous && latest > 150) {
+            setVisible(true)
+        } else {
+            setVisible(false)
+        }
+    })
 
     return (
-        <header className={`header ${visible ? 'visible' : 'hidden'}`}>
+        <motion.header
+            className="header-container"
+            variants={{
+                visible: { y: 0 },
+                hidden: { y: "-100%" },
+            }}
+            animate={visible ? "hidden" : "visible"}
+            transition={{ duration: 0.35, ease: "easeInOut" }}
+        >
             <Navbar />
             <NavLink to="/"><img className="logo-img" src={logoImage} alt="logo image" /></NavLink>
             <i className="fa fa-thin fa-cart-shopping"></i>
-        </header>
+        </motion.header>
     )
 } 
