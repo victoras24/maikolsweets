428cd42d8b1161d57768047396458bd11ab12b6f
victoras24 <fin17217@uom.edu.gr>
2023-12-17 14:43:26 +0200

    smaller toast size

--- components/ToastProvider.jsx
+++ components/ToastProvider.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const ToastProvider = ({ children }) => {
     const [toasts, setToasts] = useState([])
 
-    const open = (component, timeout = 5000) => {
+    const open = (component, timeout = 500000) => {
         const id = Date.now()
         setToasts((toasts) => [...toasts, { id, component }])
         setTimeout(() => close(id), timeout)
@@ -14,10 +14,28 @@ const ToastProvider = ({ children }) => {
 
     const openUsernameExistsError = () => {
         open(
-            <div className="toast-error">
-                <i className="fa-solid fa-circle-exclamation" />
-                <p>Username already exists</p>
-            </div>
+            <AnimatePresence>
+                <motion.div
+                    key="error-toast"
+                    className="toast-error"
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    transition={{
+                        duration: 0.3,
+                        ease: [0, 0.71, 0.2, 1.01],
+                        scale: {
+                            type: 'spring',
+                            damping: 5,
+                            stiffness: 100,
+                            restDelta: 0.001,
+                        },
+                    }}
+                >
+                    <i className="fa-solid fa-circle-exclamation" />
+                    <p>Username already exists</p>
+                </motion.div>
+            </AnimatePresence>
         )
     }
 
@@ -31,9 +49,9 @@ const ToastProvider = ({ children }) => {
             <div className="toast-container">
                 {toasts.map(({ id, component }) => (
                     <div key={id} className="toast">
-                        <button className="toast-close-button" onClick={() => close(id)}>
+                        {/* <button className="toast-close-button" onClick={() => close(id)}>
                             x
-                        </button>
+                        </button> */}
                         {component}
                     </div>
                 ))}
